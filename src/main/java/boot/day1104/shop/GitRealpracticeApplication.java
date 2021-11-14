3218872dace39010823b5dc462ce97b31b6601c6
wonseopK <wonseop77@daum.net>
2021-11-14 16:02:21 +0900

    practice start

---
+++ src/main/java/boot/day1104/shop/GitRealpracticeApplication.java
@@ -0,0 +1,13 @@
+package boot.day1104.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRealpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRealpracticeApplication.class, args);
+	}
+
+}
